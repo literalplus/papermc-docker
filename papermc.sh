261e7fe29291093a8cec0848f97a36ef4ba73f61
DaruSupaHaka <49533914+DaruSupaHaka@users.noreply.github.com>
2021-01-19 22:27:40 -0600

    Append "exec" to java command
    PaperMC does not receive the SIGTERM signal from docker when attempting to stop the container. This causes docker to kill the container after 10 seconds and the server is not shut down gracefully. Adding "exec" causes the java command to become PID 1 instead of papermc.sh, then the SIGTERM signal is sent directly to java

--- papermc.sh
+++ papermc.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-java -server -Xms${MC_RAM} -Xmx${MC_RAM} ${JAVA_OPTS} -jar ${JAR_NAME}.jar nogui
+exec java -server -Xms${MC_RAM} -Xmx${MC_RAM} ${JAVA_OPTS} -jar ${JAR_NAME}.jar nogui
